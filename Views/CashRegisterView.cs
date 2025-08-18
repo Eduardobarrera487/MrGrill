7060e7d17fc8303aa3606068bfea84359aa3e74d
Eduardobarrera487 <barreraeduardo099@gmail.com>
2025-08-17 20:10:23 -0600

    update: Avance del flujo de caja

--- Views/CashRegisterView.cs
+++ Views/CashRegisterView.cs
@@ -16,5 +16,55 @@ namespace MrGrill.Views
         {
             InitializeComponent();
         }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCaja_MouseEnter(object sender, EventArgs e)
+        {
+            btnCaja.BackColor = Color.FromArgb(217, 119, 6);
+        }
+
+        private void btnCaja_MouseLeave(object sender, EventArgs e)
+        {
+            btnCaja.BackColor = Color.FromArgb(245, 158, 11);
+        }
+
+        private void btnIngresos_MouseEnter(object sender, EventArgs e)
+        {
+            btnIngresos.BackColor = Color.FromArgb(22, 163, 74);
+        }
+
+        private void btnIngresos_MouseLeave(object sender, EventArgs e)
+        {
+            btnIngresos.BackColor = Color.FromArgb(34, 197, 94);
+        }
+
+        private void btnEgresos_MouseEnter(object sender, EventArgs e)
+        {
+            btnEgresos.BackColor = Color.FromArgb(220, 38, 38);
+        }
+
+        private void btnEgresos_MouseLeave(object sender, EventArgs e)
+        {
+            btnEgresos.BackColor = Color.FromArgb(239, 68, 68);
+        }
+
+        private void btnArqueo_MouseEnter(object sender, EventArgs e)
+        {
+            btnArqueo.BackColor = Color.FromArgb(2, 132, 199);
+        }
+
+        private void btnArqueo_MouseLeave(object sender, EventArgs e)
+        {
+            btnArqueo.BackColor = Color.FromArgb(14, 165, 233);
+        }
     }
 }
