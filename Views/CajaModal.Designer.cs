d7ad089ac64200fe95e1c3fa1e9a7e5d9f48e54a
Eduardo <barreraeduardo099@gmail.com>
2025-08-31 14:19:55 -0600

    ft: terminados los disenos de modales de ingresos y egresos

---
+++ Views/CajaModal.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace MrGrill.Views
+{
+    partial class CajaModal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fechaApertura = new RJCodeAdvance.RJControls.RJDatePicker();
+            this.txtMonto = new RJCodeAdvance.RJControls.RJTextBox();
+            this.txtObservaciones = new RJCodeAdvance.RJControls.RJTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rjButton1 = new RJCodeAdvance.RJControls.RJButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(657, 123);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(163, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gestion de Caja";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fecha de apertura:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(113, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Monto inicial:";
+            // 
+            // fechaApertura
+            // 
+            this.fechaApertura.BorderColor = System.Drawing.Color.White;
+            this.fechaApertura.BorderSize = 0;
+            this.fechaApertura.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.fechaApertura.Location = new System.Drawing.Point(318, 245);
+            this.fechaApertura.MinimumSize = new System.Drawing.Size(0, 35);
+            this.fechaApertura.Name = "fechaApertura";
+            this.fechaApertura.Size = new System.Drawing.Size(265, 35);
+            this.fechaApertura.SkinColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.fechaApertura.TabIndex = 3;
+            this.fechaApertura.TextColor = System.Drawing.Color.White;
+            this.fechaApertura.Value = new System.DateTime(2025, 8, 19, 13, 2, 10, 0);
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.BackColor = System.Drawing.SystemColors.Window;
+            this.txtMonto.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.txtMonto.BorderFocusColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(75)))), ((int)(((byte)(0)))));
+            this.txtMonto.BorderRadius = 0;
+            this.txtMonto.BorderSize = 2;
+            this.txtMonto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMonto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtMonto.Location = new System.Drawing.Point(318, 183);
+            this.txtMonto.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMonto.Multiline = false;
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.txtMonto.PasswordChar = false;
+            this.txtMonto.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txtMonto.PlaceholderText = "";
+            this.txtMonto.Size = new System.Drawing.Size(265, 35);
+            this.txtMonto.TabIndex = 4;
+            this.txtMonto.Texts = "";
+            this.txtMonto.UnderlinedStyle = false;
+            // 
+            // txtObservaciones
+            // 
+            this.txtObservaciones.BackColor = System.Drawing.SystemColors.Window;
+            this.txtObservaciones.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.txtObservaciones.BorderFocusColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(75)))), ((int)(((byte)(0)))));
+            this.txtObservaciones.BorderRadius = 0;
+            this.txtObservaciones.BorderSize = 2;
+            this.txtObservaciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtObservaciones.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtObservaciones.Location = new System.Drawing.Point(318, 310);
+            this.txtObservaciones.Margin = new System.Windows.Forms.Padding(4);
+            this.txtObservaciones.Multiline = true;
+            this.txtObservaciones.Name = "txtObservaciones";
+            this.txtObservaciones.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.txtObservaciones.PasswordChar = false;
+            this.txtObservaciones.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txtObservaciones.PlaceholderText = "";
+            this.txtObservaciones.Size = new System.Drawing.Size(265, 116);
+            this.txtObservaciones.TabIndex = 6;
+            this.txtObservaciones.Texts = "";
+            this.txtObservaciones.UnderlinedStyle = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(98, 307);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 38);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Observaciones:";
+            // 
+            // rjButton1
+            // 
+            this.rjButton1.BackColor = System.Drawing.Color.DarkGreen;
+            this.rjButton1.BackgroundColor = System.Drawing.Color.DarkGreen;
+            this.rjButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rjButton1.BorderRadius = 10;
+            this.rjButton1.BorderSize = 0;
+            this.rjButton1.FlatAppearance.BorderSize = 0;
+            this.rjButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjButton1.ForeColor = System.Drawing.Color.White;
+            this.rjButton1.Location = new System.Drawing.Point(206, 495);
+            this.rjButton1.Name = "rjButton1";
+            this.rjButton1.Size = new System.Drawing.Size(272, 54);
+            this.rjButton1.TabIndex = 7;
+            this.rjButton1.Text = "Abrir Caja";
+            this.rjButton1.TextColor = System.Drawing.Color.White;
+            this.rjButton1.UseVisualStyleBackColor = false;
+            // 
+            // CajaModal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(657, 646);
+            this.Controls.Add(this.rjButton1);
+            this.Controls.Add(this.txtObservaciones);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.fechaApertura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "CajaModal";
+            this.Text = "CajaModal";
+            this.Load += new System.EventHandler(this.CajaModal_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private RJCodeAdvance.RJControls.RJDatePicker fechaApertura;
+        private RJCodeAdvance.RJControls.RJTextBox txtMonto;
+        private RJCodeAdvance.RJControls.RJTextBox txtObservaciones;
+        private System.Windows.Forms.Label label4;
+        private RJCodeAdvance.RJControls.RJButton rjButton1;
+    }
+}
